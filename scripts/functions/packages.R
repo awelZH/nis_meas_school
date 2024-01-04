fbb7e595ba2e2810e66a23be152bfcc62d023f6e
philbosch <philippmartinbosch@gmail.com>
2024-01-04 16:28:51 +0100

    feat: package source script

---
+++ scripts/functions/packages.R
@@ -0,0 +1,15 @@
+#### INSTALL AND LOAD PACKAGES ==========================================================
+
+# install pacman package if not installed -----------------------------------------------
+suppressWarnings(if (!require("pacman")) install.packages("pacman"))
+
+# load packages and install if not installed --------------------------------------------
+pacman::p_load(cli, purrr, stringr, vroom, janitor, readr, vctrs, dplyr, fs, data.table,
+               install = TRUE,
+               update = FALSE)
+
+# show loaded packages ------------------------------------------------------------------
+cat("loaded packages\n")
+print(pacman::p_loaded())
+
+
