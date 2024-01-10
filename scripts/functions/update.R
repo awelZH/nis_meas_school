1686301ef60358cc57f6646de961b9be49255a02
Adrian Rupp <adrianrupp88@gmail.com>
2024-01-10 22:05:05 +0100

    feat: add alerts

--- scripts/functions/update.R
+++ scripts/functions/update.R
@@ -5,60 +5,68 @@
 #' die Messwerte Files (Rohdaten Messwerte & Aufbereitete Messwerte) aktualisiert.
 #' Das OGD Dataset muss bereits in der Metadatenverwaltung angelegt sein und die richtige ID in den Parameter (siehe weiter unten) angegeben werden.
 #' Es braucht Schreibrechte im MDV, welche mit den dazugehörigen Username Passwort Kombination möglich ist (siehe nächste Linie). Ausserdem braucht man einen Token, um das neuste zhMetadatenAPI Package von Github herunterzuladen.
-#' Bitte beachte, dass in deinem .Renviron File die Variabeln: 'mdv_user', 'mdv_pw' und 'ZH_METADATEN_API_TOKEN' mit dazugehörigen Werten vorhanden ist.
-#' @param dataset_id numeric, Dataset ID aus der Metadatenverwaltung
-#' @param testmode boolean, testmode = FALSE bedeutet, dass die Files auf das Produktive MDV geladen werden
-#' @param titel_rohdaten_mdv Titel der Rohdaten Ressourcen im MDV als String.
-#' @param titel_aufbereitete_daten_mdv Titel der Aufbereitete Daten Ressourcen im MDV als String.
-#' @param mdv_user Username als String (in der Regel die Emailadresse des Users vom MDV)
-#' @param mdv_pw Password als String für den Zugang des MDV
-#' @param path_rohdaten_messwerte Datenpfad als String zum Rohdaten File. Dieses File wird in dieser Funktion im MDV hochgeladen
-#' @param path_aufbereitetem_file Datenpfad als String zum Aufbereiteten File. Dieses File wird in dieser Funktion im MDV hochgeladen
+#' Bitte beachte, dass im .Renviron File die Variabeln: 'mdv_user', 'mdv_pw' und 'ZH_METADATEN_API_TOKEN' mit dazugehörigen Werten vorhanden ist.
 #'
 #'
-#' @examples update(dataset_id = 6436, testmode = FALSE, titel_rohdaten_mdv = "Rohdaten Messwerte", titel_aufbereitete_daten_mdv = "Aufbereitete Messwerte", mdv_user = Sys.getenv("mdv_user"), mdv_pw = Sys.getenv("mdv_pw"), path_rohdaten_messwerte = "data/temp/rohdaten_messwerte.zip", path_aufbereitetem_file = 'data/temp/aufbereitete_messwerte.csv')
+#' @examples update()
 #'
-update <- function(dataset_id, testmode, titel_rohdaten_mdv, titel_aufbereitete_daten_mdv, mdv_user, mdv_pw, path_rohdaten_messwerte, path_aufbereitem_file){
+update <- function(){
+
+  ##---------------------------------------------------------------------------
+  # 0. Parameter setzen und überprüfen
+  ##---------------------------------------------------------------------------
+  cli::cli_alert_info("Starte Update Prozess")
+
+  dataset_id = 6436
+  testmode = FALSE # boolean, testmode = FALSE bedeutet, dass die Files auf das Produktive MDV geladen werden
+  titel_rohdaten_mdv = "Rohdaten Messwerte" # Titel der Rohdaten Ressourcen im MDV als String.
+  titel_aufbereitete_daten_mdv = "Aufbereitete Messwerte" # Titel der Aufbereitete Daten Ressourcen im MDV als String.
+  mdv_user = Sys.getenv("mdv_user") # Username als String (in der Regel die Emailadresse des Users vom MDV)
+  mdv_pw = Sys.getenv("mdv_pw") # Password als String für den Zugang des MDV
+  path_rohdaten_messwerte = "data/temp/load/rohdaten_messwerte.zip" # Datenpfad (lokal) als String zum Rohdaten Zip File
+  path_aufbereitetem_file = 'data/temp/load/aufbereitete_messwerte.csv' # Datenpfad (lokal) als String zum Aufbereiteten CSV File
 
   # Überprüfe, ob die Argumente der Funktion sind im richtigen Datenformat. Ansonsten breche ab und werfe Fehlermeldung
-  stopifnot("`dataset_id` must be numeric" = is.numeric(dataset_id))
-  stopifnot("`testmode` must be a boolean" = is.logical(testmode))
-  stopifnot("`titel_rohdaten_mdv` must be a character." = is.character(titel_rohdaten_mdv))
-  stopifnot("`titel_aufbereitete_daten_mdv` must be a character." = is.character(titel_aufbereitete_daten_mdv))
-  stopifnot("`mdv_user` must be a character." = is.character(mdv_user))
-  stopifnot("`mdv_pw` must be a character." = is.character(mdv_pw))
-  stopifnot("`path_rohdaten_messwerte` must be a character." = is.character(path_rohdaten_messwerte))
-  stopifnot("`path_aufbereitem_file` must be a character." = is.character(path_aufbereitetem_file))
+  assertthat::assert_that(msg = "dataset_id` must be numeric" , is.numeric(dataset_id))
+  assertthat::assert_that(msg = "testmode` must be a boolean" , is.logical(testmode))
+  assertthat::assert_that(msg = "titel_rohdaten_mdv` must be a character." , is.character(titel_rohdaten_mdv))
+  assertthat::assert_that(msg = "titel_aufbereitete_daten_mdv` must be a character." , is.character(titel_aufbereitete_daten_mdv))
+  assertthat::assert_that(msg = "mdv_user` must be a character." , is.character(mdv_user))
+  assertthat::assert_that(msg = "mdv_pw` must be a character." , is.character(mdv_pw))
+  assertthat::assert_that(msg = "path_rohdaten_messwerte` must be a character." , is.character(path_rohdaten_messwerte))
+  assertthat::assert_that(msg = "path_aufbereitem_file` must be a character." , is.character(path_aufbereitetem_file))
 
   # Überprüfe ob die lokalen Files unter den gewählten Pfäden exisitieren
-  stopifnot("Unter dem gewählten Pfad befindet sich kein Rohdaten File oder der Zugriff auf das File ist nicht möglich" = file.exists(path_rohdaten_messwerte))
-  stopifnot("Unter dem gewählten Pfad befindet sich kein Aufbereitetes Data File oder der Zugriff auf das File ist nicht möglich" = file.exists(path_aufbereitetem_file))
+  assertthat::assert_that(msg = "Unter dem gewählten Pfad befindet sich kein Rohdaten File oder der Zugriff auf das File ist nicht möglich" , file.exists(path_rohdaten_messwerte))
+  assertthat::assert_that(msg = "Unter dem gewählten Pfad befindet sich kein Aufbereitetes Data File oder der Zugriff auf das File ist nicht möglich" , file.exists(path_aufbereitetem_file))
 
-##---------------------------------------------------------------------------
+  ##---------------------------------------------------------------------------
+  # 1. Beziehe Ressourcen IDs für die Messwerte Ressourcen vom MDV
+  ##---------------------------------------------------------------------------
 
-# 1. Beziehe Ressourcen IDs für die Messwerte Ressourcen vom MDV
-
-# Hole alle Ressourcen Information vom MDV für die gewählte Dataset_ID
-metadaten_ressourcen_mdv  <-  zhMetadatenAPI::get_distributions(dataset_id, mdv_user, mdv_pw, testmode = testmode, verbose = FALSE) %>%
+  # Hole alle Ressourcen Information vom MDV für die gewählte Dataset_ID
+  metadaten_ressourcen_mdv  <-  zhMetadatenAPI::get_distributions(dataset_id, mdv_user, mdv_pw, testmode = testmode, verbose = FALSE) %>%
   # Status der Ressourcen muss grösser als 0 sein
   dplyr::filter(STATUS >0)
 
-# Ressource ID der Rohdaten Ressource
-ressourcen_id_rohdaten <- metadaten_ressourcen_mdv[metadaten_ressourcen_mdv$LABEL == titel_rohdaten_mdv, "ID"][[1]]
-stopifnot("Es konnte keine Rohdaten ID vom MDV mit diesem Titel unter der gewählten Dataset_ID gefunden werden" = is.numeric(ressourcen_id_rohdaten))
+  # Ressource ID der Rohdaten Ressource
+  ressourcen_id_rohdaten <- metadaten_ressourcen_mdv[metadaten_ressourcen_mdv$LABEL == titel_rohdaten_mdv, "ID"][[1]]
+  assertthat::assert_that(msg = "Es konnte keine Rohdaten ID vom MDV mit diesem Titel unter der gewählten Dataset_ID gefunden werden" , is.numeric(ressourcen_id_rohdaten))
 
-# Ressource ID der Aufbereitete Ressource
-ressourcen_id_aufbereitete_daten <- metadaten_ressourcen_mdv[metadaten_ressourcen_mdv$LABEL == titel_aufbereitete_daten_mdv, "ID"][[1]]
-stopifnot("Es konnte keine Aufbereitete Daten ID vom MDV mit diesem Titel unter der gewählten Dataset_ID gefunden werden" = is.numeric(ressourcen_id_aufbereitete_daten))
+  # Ressource ID der Aufbereitete Ressource
+  ressourcen_id_aufbereitete_daten <- metadaten_ressourcen_mdv[metadaten_ressourcen_mdv$LABEL == titel_aufbereitete_daten_mdv, "ID"][[1]]
+  assertthat::assert_that(msg = "Es konnte keine Aufbereitete Daten ID vom MDV mit diesem Titel unter der gewählten Dataset_ID gefunden werden" , is.numeric(ressourcen_id_aufbereitete_daten))
 
-## ----------------------------------------------------------------------------------------------------------------
-
-# 2. Upload
+  ##---------------------------------------------------------------------------
+  # 2. Upload der Ressourcen in den MDV
+  ##---------------------------------------------------------------------------
 
   # Erstelle nächstes erwartetes Datum, wo Daten aktualisiert werden. In der Regel ist dies im nächsten Jahr
   next_expected_update_date <- as.character(paste(as.numeric(format(Sys.Date(), "%Y")) + 1, format(Sys.Date(), "%m-%d"), sep = "-"))
 
-  # Update Rohdaten Messwerte
+  ##---------------------------------------------------------------------------
+  # 2.1 Update Rohdaten Messwerte
+  ##---------------------------------------------------------------------------
 
   # Lade File in den MDV hoch
   try(zhMetadatenAPI::update_distribution(user=mdv_user,
@@ -73,7 +81,9 @@ stopifnot("Es konnte keine Aufbereitete Daten ID vom MDV mit diesem Titel unter 
                                           right_id = 2,
                                           testmode = testmode))
 
-  # Update Aufbereitete Messwerte
+  ##---------------------------------------------------------------------------
+  # 2.2 Update Aufbereitetes Messwerte File
+  ##---------------------------------------------------------------------------
 
   # Erstelle die Datum Metadaten aus dem Aufbereitetem Data File und dem aktuellen Datum
   min_date <- as.character(paste0(min(readr::read_csv(path_aufbereitetem_file)$Jahr), "-01-01"))
@@ -95,6 +105,8 @@ stopifnot("Es konnte keine Aufbereitete Daten ID vom MDV mit diesem Titel unter 
                                           right_id = 2,
                                           testmode = testmode))
 
+  cli::cli_alert_info("Update Skript ist erfolgreich durchgelaufen")
+
 }
-## ----------------------------------------------------------------------------------------------------------------
+##---------------------------------------------------------------------------
 
