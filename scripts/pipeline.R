d091196457c96d27ff2d7325b1e85d5ab7962f76
Adrian Rupp <adrianrupp88@gmail.com>
2024-01-25 12:13:33 +0100

    feat: add full and delta load variables, that it can be used in different functions

--- scripts/pipeline.R
+++ scripts/pipeline.R
@@ -9,14 +9,15 @@ update_project_from_github()
 #Set path to local files
 path_rohdaten_topfolder = "~/file-server/file-server/08_DS/01_Projekte/AWEL/2023_Schulhausmessung/"
 
-
+full_load = FALSE
+delta_load = TRUE
 # run the pipeline
 
 # Call the extract function
-extract(full_load = TRUE, delta_load = FALSE, path_rohdaten_topfolder = path_rohdaten_topfolder)
+extract(full_load = full_load, delta_load = delta_load, path_rohdaten_topfolder = path_rohdaten_topfolder)
 
 # Call the transform function
-transform()
+transform(full_load = full_load)
 
 # Call the update function
 update()
