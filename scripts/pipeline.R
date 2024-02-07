e80c8e578a5c9b2f8b4041f55f96d93beab33e15
Adrian Rupp <adrianrupp88@gmail.com>
2024-02-07 18:06:02 +0100

    feat: add setwd statement

--- scripts/pipeline.R
+++ scripts/pipeline.R
@@ -6,8 +6,12 @@ source_files(path = "scripts/functions")
 # pull the newest changes of git-repo
 update_project_from_github()
 
+#Set path to home directory (Path to local nis_meas_shool github repo)
+setwd("")
+
 #Set path to local files
 path_rohdaten_topfolder <- "~/file-server/file-server/08_DS/01_Projekte/AWEL/2023_Schulhausmessung/"
+
 
 full_load <- FALSE
 delta_load <- TRUE
