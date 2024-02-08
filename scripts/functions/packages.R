3db2e21d2ab7ea5a1e4271a47ccea0e31d6db096
Thomas Knecht <thomas.knecht@zh.ch>
2024-02-08 10:08:08 +0100

    debug packages

--- scripts/functions/packages.R
+++ scripts/functions/packages.R
@@ -5,9 +5,12 @@ suppressWarnings(if (!require("pacman")) install.packages("pacman"))
 
 # load packages and install if not installed --------------------------------------------
 pacman::p_load(cli, purrr, stringr, vroom, janitor, readr, vctrs, dplyr, fs, data.table,
-               tibble, assertthat, zoo,
-               install = FALSE,
+               tibble, assertthat, zoo,archive,httr2,lubridate,stringr,tidyr,
+               install = TRUE,
                update = FALSE)
+
+remotes::install_github("statistikZH/zhMetadatenAPI",
+                        auth_token=Sys.getenv("ZH_METADATEN_API_TOKEN"))
 
 # show loaded packages ------------------------------------------------------------------
 cat("loaded packages\n")
