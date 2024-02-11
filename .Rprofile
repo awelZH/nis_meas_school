a14eb1a522668a17c0054d96c368aa1fe54f0d2c
Adrian Rupp <adrianrupp88@gmail.com>
2024-02-11 20:12:17 +0100

    feat: update

--- .Rprofile
+++ .Rprofile
@@ -1,8 +1,12 @@
+# source_files <- function(path, pattern = "\\.R$") {
+#   # List all files in the specified path
+#   all_files <- list.files(path, pattern = pattern, full.names = TRUE)
+#
+#   # Source each file
+#   sapply(all_files, source)
+# }
 
-source_files <- function(path, pattern = "\\.R$") {
-  # List all files in the specified path
-  all_files <- list.files(path, pattern = pattern, full.names = TRUE)
+# Installiere zhMetadatenAP Package von Github
+remotes::install_github("statistikZH/zhMetadatenAPI", auth_token=Sys.getenv("ZH_METADATEN_API_TOKEN"))
 
-  # Source each file
-  sapply(all_files, source)
-}
+cat("Bitte überprüfe, ob die Pfäde ('path_rohdaten_topfolder' und 'path_wd') und Variabeln ('full_load' und 'delta_load') im scripts/pipeline.R Skript richtig gesetzt sind. Passe sie vor dem Ausführen des Skripts ggfs. noch an." )
--- scripts/pipeline.R
+++ scripts/pipeline.R
@@ -1,34 +1,22 @@
-#### INSTALL AND LOAD PACKAGES ==========================================================
-
-# install pacman package if not installed -----------------------------------------------
-suppressWarnings(if (!require("pacman")) install.packages("pacman"))
-
-# load packages and install if not installed --------------------------------------------
-pacman::p_load(cli, purrr, stringr, vroom, janitor, readr, vctrs, dplyr, fs, data.table,
-               tibble, assertthat, zoo,archive,httr2,lubridate,stringr,tidyr,
-               install = TRUE,
-               update = FALSE)
-
-# Installiere zhMetadatenAP Package von Github
-remotes::install_github("statistikZH/zhMetadatenAPI", auth_token=Sys.getenv("ZH_METADATEN_API_TOKEN"))
-
-# show loaded packages ------------------------------------------------------------------
-cat("loaded packages\n")
-print(pacman::p_loaded())
-
-devtools::load_all(".")
-
-#### Führe Berechnung und OGD Upload durch ==========================================================
-
-# Downloade aktuellste Version des Codes von Github
-update_project_from_github()
+#### Definiere Variablen ==========================================================
 
 #Definiere Pfad, wo Rohdaten lokal liegen
 path_rohdaten_topfolder <- "/home/file-server/08_DS/01_Projekte/AWEL/2023_Schulhausmessung/"
 
+# Definiere Working directory
+path_wd <- "~/gitrepos/nis_meas_school"
+setwd(path_wd)
+
 # Entscheide ob ein Full oder Delta Load gemacht werden sollte
-full_load <- FALSE
-delta_load <- TRUE
+full_load <- T
+delta_load <- F
+
+# Downloade aktuellste Version des Codes von Github
+update_project_from_github()
+
+devtools::load_all()
+#source_files("R")
+#### Führe Berechnung und OGD Upload durch ==========================================================
 
 # Lade alle benötigten Daten
 extract(full_load = full_load, delta_load = delta_load, path_rohdaten_topfolder = path_rohdaten_topfolder)
