b9f8a8070e8532ebf8941f809f27e411a0e4139d
Adrian Rupp <adrianrupp88@gmail.com>
2024-02-22 08:05:20 +0100

    fix: remove get_distribution function

--- R/update.R
+++ R/update.R
@@ -16,20 +16,20 @@ update <- function(){
   ##---------------------------------------------------------------------------
   cli::cli_alert_info("Starte Update Prozess")
 
-  dataset_id = 6436
-  testmode = FALSE # boolean, testmode = FALSE bedeutet, dass die Files auf das Produktive MDV geladen werden
-  titel_rohdaten_mdv = "Rohdaten Messwerte" # Titel der Rohdaten Ressourcen im MDV als String.
-  titel_aufbereitete_daten_mdv = "Aufbereitete Messwerte" # Titel der Aufbereitete Daten Ressourcen im MDV als String.
-  mdv_user = Sys.getenv("mdv_user") # Username als String (in der Regel die Emailadresse des Users vom MDV)
-  mdv_pw = Sys.getenv("mdv_pw") # Password als String für den Zugang des MDV
-  path_rohdaten_messwerte = "inst/extdata/temp/load/rohdaten_messwerte.zip" # Datenpfad (lokal) als String zum Rohdaten Zip File
-  path_aufbereitetem_file = "inst/extdata/temp/load/aufbereitete_messwerte.csv" # Datenpfad (lokal) als String zum Aufbereiteten CSV File
+  dataset_id <- 6436
+  testmode <- FALSE # boolean, testmode = FALSE bedeutet, dass die Files auf das Produktive MDV geladen werden
+  ressourcen_id_aufbereitete_daten <-4983 # ID der Rohdaten Ressourcen im MDV
+  ressourcen_id_rohdaten <- 5003 # ID der Aufbereitete Daten Ressourcen im MDV
+  mdv_user <- Sys.getenv("mdv_user") # Username als String (in der Regel die Emailadresse des Users vom MDV)
+  mdv_pw <- Sys.getenv("mdv_pw") # Password als String für den Zugang des MDV
+  path_rohdaten_messwerte <- "inst/extdata/temp/load/rohdaten_messwerte.zip" # Datenpfad (lokal) als String zum Rohdaten Zip File
+  path_aufbereitetem_file <- "inst/extdata/temp/load/aufbereitete_messwerte.csv" # Datenpfad (lokal) als String zum Aufbereiteten CSV File
 
   # Überprüfe, ob die Argumente der Funktion sind im richtigen Datenformat. Ansonsten breche ab und werfe Fehlermeldung
   assertthat::assert_that(msg = "dataset_id` must be numeric" , is.numeric(dataset_id))
   assertthat::assert_that(msg = "testmode` must be a boolean" , is.logical(testmode))
-  assertthat::assert_that(msg = "titel_rohdaten_mdv` must be a character." , is.character(titel_rohdaten_mdv))
-  assertthat::assert_that(msg = "titel_aufbereitete_daten_mdv` must be a character." , is.character(titel_aufbereitete_daten_mdv))
+  assertthat::assert_that(msg = "ressourcen_id_aufbereitete_daten` must be numeric" , is.numeric(ressourcen_id_aufbereitete_daten))
+  assertthat::assert_that(msg = "ressourcen_id_rohdaten` must be numeric" , is.numeric(ressourcen_id_rohdaten))
   assertthat::assert_that(msg = "MDV Username fehlt im .Renviron File. Füge den Username der Variable mdv_user hinzu." , mdv_user != "")
   assertthat::assert_that(msg = "MDV Passwort fehlt im .Renviron File. Füge das Passwort der Variable mdv_pw hinzu." , mdv_pw != "")
   assertthat::assert_that(msg = "mdv_user` must be a character." , is.character(mdv_user))
@@ -40,23 +40,7 @@ update <- function(){
   # Überprüfe ob die lokalen Files unter den gewaehlten Pfaeden exisitieren
   assertthat::assert_that(msg = "Unter dem gewaehlten Pfad befindet sich kein Rohdaten File oder der Zugriff auf das File ist nicht moeglich" , file.exists(path_rohdaten_messwerte))
   assertthat::assert_that(msg = "Unter dem gewaehlten Pfad befindet sich kein Aufbereitetes Data File oder der Zugriff auf das File ist nicht moeglich" , file.exists(path_aufbereitetem_file))
-
-  ##---------------------------------------------------------------------------
-  # 1. Beziehe Ressourcen IDs für die Messwerte Ressourcen vom MDV
-  ##---------------------------------------------------------------------------
-
-  # Hole alle Ressourcen Information vom MDV für die gewaehlte Dataset_ID
-  metadaten_ressourcen_mdv  <-  zhMetadatenAPI::get_distributions(dataset_id, mdv_user, mdv_pw, testmode = testmode, verbose = FALSE) %>%
-  # Status der Ressourcen muss groesser als 0 sein
-  dplyr::filter(STATUS >0)
-
-  # Ressource ID der Rohdaten Ressource
-  ressourcen_id_rohdaten <- metadaten_ressourcen_mdv[metadaten_ressourcen_mdv$LABEL == titel_rohdaten_mdv, "ID"][[1]]
   assertthat::assert_that(msg = "Es konnte keine Rohdaten ID vom MDV mit diesem Titel unter der gewaehlten Dataset_ID gefunden werden" , is.numeric(ressourcen_id_rohdaten))
-
-  # Ressource ID der Aufbereitete Ressource
-  ressourcen_id_aufbereitete_daten <- metadaten_ressourcen_mdv[metadaten_ressourcen_mdv$LABEL == titel_aufbereitete_daten_mdv, "ID"][[1]]
-  assertthat::assert_that(msg = "Es konnte keine Aufbereitete Daten ID vom MDV mit diesem Titel unter der gewaehlten Dataset_ID gefunden werden" , is.numeric(ressourcen_id_aufbereitete_daten))
 
   ##---------------------------------------------------------------------------
   # 2. Upload der Ressourcen in den MDV
