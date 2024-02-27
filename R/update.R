0040d6715350c0339e369a3b9d5c27007beade1a
Adrian Rupp <adrianrupp88@gmail.com>
2024-02-27 11:00:23 +0100

    fix: Ersetze ü mit ue

--- R/update.R
+++ R/update.R
@@ -1,7 +1,7 @@
-#' Update MDV Messwerte Ressourcen der Immissionsmessungen NIS im Kanton Zürich als OGD
+#' Update MDV Messwerte Ressourcen der Immissionsmessungen NIS im Kanton Zuerich als OGD
 #'
 #' Dieses Funktion eignet sich um die OGD Schulhausmessungsdaten (nur Messdaten) zu aktualisieren.
-#' Achtung: Zur Aktualiserung des 'Messorte' CSV File -> über das GUI des MDV hochladen. Mit diesem Skript werden nur
+#' Achtung: Zur Aktualiserung des 'Messorte' CSV File -> ueber das GUI des MDV hochladen. Mit diesem Skript werden nur
 #' die Messwerte Files (Rohdaten Messwerte & Aufbereitete Messwerte) aktualisiert.
 #' Das OGD Dataset muss bereits in der Metadatenverwaltung angelegt sein und die richtige ID in den Parameter (siehe weiter unten) angegeben werden.
 #' Es braucht Schreibrechte im MDV, welche mit den dazugehoerigen Username Passwort Kombination moeglich ist (siehe naechste Linie). Ausserdem braucht man einen Token, um das neuste zhMetadatenAPI Package von Github herunterzuladen.
@@ -12,7 +12,7 @@
 update <- function(){
 
   ##---------------------------------------------------------------------------
-  # 0. Parameter setzen und überprüfen
+  # 0. Parameter setzen und ueberpruefen
   ##---------------------------------------------------------------------------
   cli::cli_alert_info("Starte Update Prozess")
 
@@ -20,22 +20,22 @@ update <- function(){
   ressourcen_id_aufbereitete_daten <-4983 # ID der Rohdaten Ressourcen im MDV
   ressourcen_id_rohdaten <- 5003 # ID der Aufbereitete Daten Ressourcen im MDV
   mdv_user <- Sys.getenv("mdv_user") # Username als String (in der Regel die Emailadresse des Users vom MDV)
-  mdv_pw <- Sys.getenv("mdv_pw") # Password als String für den Zugang des MDV
+  mdv_pw <- Sys.getenv("mdv_pw") # Password als String fuer den Zugang des MDV
   path_rohdaten_messwerte <- "inst/extdata/temp/load/rohdaten_messwerte.zip" # Datenpfad (lokal) als String zum Rohdaten Zip File
   path_aufbereitetem_file <- "inst/extdata/temp/load/aufbereitete_messwerte.csv" # Datenpfad (lokal) als String zum Aufbereiteten CSV File
 
-  # Überprüfe, ob die Argumente der Funktion sind im richtigen Datenformat. Ansonsten breche ab und werfe Fehlermeldung
+  # ueberpruefe, ob die Argumente der Funktion sind im richtigen Datenformat. Ansonsten breche ab und werfe Fehlermeldung
   assertthat::assert_that(msg = "testmode` must be a boolean" , is.logical(testmode))
   assertthat::assert_that(msg = "ressourcen_id_aufbereitete_daten` must be integer" , is.count(ressourcen_id_aufbereitete_daten))
   assertthat::assert_that(msg = "ressourcen_id_rohdaten` must be integer" , is.count(ressourcen_id_rohdaten))
-  assertthat::assert_that(msg = "MDV Username fehlt im .Renviron File. Füge den Username der Variable mdv_user hinzu." , mdv_user != "")
-  assertthat::assert_that(msg = "MDV Passwort fehlt im .Renviron File. Füge das Passwort der Variable mdv_pw hinzu." , mdv_pw != "")
+  assertthat::assert_that(msg = "MDV Username fehlt im .Renviron File. Fuege den Username der Variable mdv_user hinzu." , mdv_user != "")
+  assertthat::assert_that(msg = "MDV Passwort fehlt im .Renviron File. Fuege das Passwort der Variable mdv_pw hinzu." , mdv_pw != "")
   assertthat::assert_that(msg = "mdv_user` must be a character." , is.character(mdv_user))
   assertthat::assert_that(msg = "mdv_pw` must be a character." , is.character(mdv_pw))
   assertthat::assert_that(msg = "path_rohdaten_messwerte` must be a character." , is.character(path_rohdaten_messwerte))
   assertthat::assert_that(msg = "path_aufbereitem_file` must be a character." , is.character(path_aufbereitetem_file))
 
-  # Überprüfe ob die lokalen Files unter den gewaehlten Pfaeden exisitieren
+  # ueberpruefe ob die lokalen Files unter den gewaehlten Pfaeden exisitieren
   assertthat::assert_that(msg = "Unter dem gewaehlten Pfad befindet sich kein Rohdaten File oder der Zugriff auf das File ist nicht moeglich" , file.exists(path_rohdaten_messwerte))
   assertthat::assert_that(msg = "Unter dem gewaehlten Pfad befindet sich kein Aufbereitetes Data File oder der Zugriff auf das File ist nicht moeglich" , file.exists(path_aufbereitetem_file))
   assertthat::assert_that(msg = "Es konnte keine Rohdaten ID vom MDV mit diesem Titel unter der gewaehlten Dataset_ID gefunden werden" , is.numeric(ressourcen_id_rohdaten))
